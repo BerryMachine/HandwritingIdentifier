f852a1a25b682701186ea74ffaffd150867ffca9
Jason He <jasonryan.he@gmail.com>
2025-06-30 19:52:49 -0400

    commit readme

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 # Recreation of 3b1b handwritten digit identifier from scratch
 
+import struct
 import numpy as np
+
+def read_images(file):
+    # Open in binary
+    with open(file, 'rb') as f:
+        # Big-endian, 28x28 = 784, header 16 bytes; 4:filetype, 4:#imgs, 4:rows, 4:cols
+        magic, n_imgs, rows, cols = struct.unpack('>IIII', f.read(16))
+        assert magic == 2051, f'Invalid {magic}' # check file type
+
+        img_data = np.frombuffer(f.read(), dtype=np.uint8) # convert to 0-255
+        images = img_data.reshape(n_imgs, rows, cols)
+        return images
 
